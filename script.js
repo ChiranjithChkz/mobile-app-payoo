a045a20bc68a24d4bc708d5afdc041d4bf21a06a
ChiranjithChkz <chiranjithchakma09@gmail.com>
2025-08-29 02:50:45 +0600

    making interective with js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
  // login button functionality
  document.getElementById('loginButton').addEventListener('click', function(e){
     e.preventDefault();
-    console.log('Login button clicked');
-    console.log(e);
+    const mobileNumber = 12345678910;
+    const pinNumber = 1234;
+    const mobileNumberValue = document.getElementById("mobile-number").value;
+    const mobileNumberValueConverted = parseInt(mobileNumberValue);
+
+    const pinNumberValue = document.getElementById('pin-number').value
+    const pinNUmberValueConverted = parseInt(pinNumberValue);
+
+    console.log(mobileNumberValueConverted, pinNUmberValueConverted);
+
+    if(mobileNumberValueConverted== mobileNumber && pinNUmberValueConverted == pinNumber)
+    {
+        window.location.href="/home.html";
+    }
+    else{
+       alert("Invalid Credential");
+    }
  })
