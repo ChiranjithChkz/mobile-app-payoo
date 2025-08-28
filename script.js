4c3bbaaecfc2a6db4393a674c3d28a748e225721
ChiranjithChkz <chiranjithchakma09@gmail.com>
2025-08-29 02:19:54 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+ // login button functionality
+ document.getElementById('loginButton').addEventListener('click', function(e){
+    e.preventDefault();
+    console.log('Login button clicked');
+    console.log(e);
+ })
